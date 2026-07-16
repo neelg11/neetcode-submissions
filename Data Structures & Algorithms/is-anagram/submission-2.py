5526a06f8a0914e9c9f202f1ae419ce0a902afd4
Indraneel Ghosh <indraneel.g11@gmail.com>
2026-07-16 19:56:26 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a=[0]*26
+        for i in s:
+            a[(ord(i)-ord('a'))]+=1
+        for i in t:
+            a[(ord(i)-ord('a'))]-=1
+        for i in a:
+            if i!=0: return False
+        return True
