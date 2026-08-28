50b90b07705c053662baa3fa01a207c0aa66625f
Indraneel Ghosh <indraneel.g11@gmail.com>
2026-08-28 15:26:34 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #checking box
+        check_rows=[[0]*10 for _ in range(9)]
+        check_col=[[0]*10  for _ in range(9)] 
+        for x in range(0,7,3):
+            for y in range(0,7,3):
+                visited=[0]*10
+                for i in range(3):
+                    for j in range(3):
+                        if(board[x+i][y+j]=='.'): continue
+                        num=int(board[x+i][y+j])
+                        if visited[num]: return False
+                        visited[num]=1 
+
+                        if check_rows[x+i][num] or check_col[y+j][num]: return False
+                        check_rows[x+i][num]=1
+                        check_col[y+j][num]=1
+
+        return True
